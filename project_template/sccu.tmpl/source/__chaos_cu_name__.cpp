b0cbf3bb9f0c3e34164405f5ffc4a1b695ca92a1
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2013-09-03 15:35:09 +0200

    fixed some issues on the rtcu creation script and added the script for the creation of the Slow Control contorl unit
    Change-Id: I9d6f3305ddbfa65bcba8b09a4f5f534b6b399294

---
+++ project_template/sccu.tmpl/source/__chaos_cu_name__.cpp
@@ -0,0 +1,70 @@
+//
+//  __chaos_cu_name__.cpp
+//  ControlUnitTest
+//
+//  Created by Claudio Bisegni on 7/20/13.
+//  Copyright (c) 2013 INFN. All rights reserved.
+//
+
+#include "__chaos_cu_name__.h"
+#include "DefaultCommand.h"
+
+using namespace chaos::common::data;
+
+using namespace chaos::cu::control_manager::slow_command;
+using namespace chaos::cu::driver_manager::driver;
+
+
+/*
+ Construct a new CU with an identifier
+ */
+__chaos_cu_name__::__chaos_cu_name__(string &customDeviceID) {
+    _deviceID = customDeviceID;
+}
+
+__chaos_cu_name__::~__chaos_cu_name__() {
+	
+}
+
+/*
+ Return the default configuration
+ */
+void __chaos_cu_name__::unitDefineActionAndDataset() throw(CException) {
+    //set the base information
+    RangeValueInfo rangeInfoTemp;
+    //cuSetup.addStringValue(CUDefinitionKey::CS_CM_CU_DESCRIPTION, "This is a beautifull CU");
+    
+    //add managed device di
+    setDeviceID(_deviceID);
+    
+    //install a command
+    installCommand<DefaultCommand>("default_command");
+}
+
+void __chaos_cu_name__::defineSharedVariable() {
+}
+
+void __chaos_cu_name__::unitDefineDriver(std::vector<cu_driver::DrvRequestInfo>& neededDriver) {
+	cu_driver::DrvRequestInfo drv1 = {"DummyDriver","1.0.0","url_host:port"};
+	neededDriver.push_back(drv1);
+}
+
+// Abstract method for the initialization of the control unit
+void __chaos_cu_name__::unitInit() throw(CException) {
+	
+}
+
+// Abstract method for the start of the control unit
+void __chaos_cu_name__::unitStart() throw(CException) {
+	
+}
+
+// Abstract method for the stop of the control unit
+void __chaos_cu_name__::unitStop() throw(CException) {
+	
+}
+
+// Abstract method for the deinit of the control unit
+void __chaos_cu_name__::unitDeinit() throw(CException) {
+	
+}
