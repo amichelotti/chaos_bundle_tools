a992b01dc074375fe413d2106e51aefb750ab286
michelo <andrea.michelotti@gmail.com>
2014-05-12 15:30:07 +0200

    compile 1.55 for linux, gcc 4.8 dont compile boost atomic 1.54

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -13,7 +13,13 @@ export CHAOS_FRAMEWORK=$CHAOS_BUNDLE/chaosframework
 export PATH=$PATH:$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin
 export DYLD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
 export LD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
-export CHAOS_BOOST_VERSION=54
+
+if [ $(uname -s) == "Linux" ]; then
+    export CHAOS_BOOST_VERSION=55
+else
+    export CHAOS_BOOST_VERSION=54
+fi;
+
 
 if [ "$CHAOS_TARGET" == "BBB" ]; then
     export LMEM_VERSION=1.0.18
