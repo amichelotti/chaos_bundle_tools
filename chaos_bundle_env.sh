e1465b2e20d3117b1085a63bc19d8bddc2f0e10c
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2014-06-06 15:17:53 +0200

    before the path of installation, then others

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -10,7 +10,7 @@ export CHAOS_BUNDLE="$(dirname "$SCRIPTPATH")"
 #boostrap !CHAOS Framework in development mode
 export CHAOS_DEVELOPMENT="YES"
 export CHAOS_FRAMEWORK=$CHAOS_BUNDLE/chaosframework
-export PATH=$PATH:$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin
+export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 export DYLD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
 export LD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
 
