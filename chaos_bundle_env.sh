709d8f1e0fb08efcf74c6eb1da09c6d70fdc2f7d
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2014-09-15 17:26:50 +0200

    update for new mongodb driver

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -14,7 +14,7 @@ export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 export DYLD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
 export LD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib:$LD_LIBRARY_PATH
 #set default compile lib
-export CHAOS_LINK_LIBRARY="boost_program_options boost_system boost_thread boost_chrono boost_regex boost_log boost_log_setup boost_filesystem memcached zmq uv dl"
+export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log boost_log_setup boost_filesystem memcached zmq uv dl"
 
 if [ $(uname -s) == "Linux" ]; then
     export CHAOS_BOOST_VERSION=55
@@ -22,8 +22,8 @@ else
     export CHAOS_BOOST_VERSION=53
 fi;
 
-#export MONGO_VERSION=r2.6.1
-export MONGO_VERSION=26compat
+#export MONGO_VERSION=26compat
+export MONGO_VERSION=legacy-1.0.0-rc0
 export LIB_EVENT_VERSION=release-2.1.4-alpha
 
 if [ "$CHAOS_TARGET" == "BBB" ]; then
