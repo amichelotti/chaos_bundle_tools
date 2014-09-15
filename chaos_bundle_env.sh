9c1ff4c213ac35e8880449f2608ccc3672d89e32
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2014-09-15 10:31:47 +0200

    fixed libevent version, and mongo on 26 compat

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -12,7 +12,7 @@ export CHAOS_DEVELOPMENT="YES"
 export CHAOS_FRAMEWORK=$CHAOS_BUNDLE/chaosframework
 export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 export DYLD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
-export LD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib
+export LD_LIBRARY_PATH=$CHAOS_BUNDLE/usr/local/lib:$LD_LIBRARY_PATH
 #set default compile lib
 export CHAOS_LINK_LIBRARY="boost_program_options boost_system boost_thread boost_chrono boost_regex boost_log boost_log_setup boost_filesystem memcached zmq uv dl"
 
@@ -22,7 +22,8 @@ else
     export CHAOS_BOOST_VERSION=53
 fi;
 
-export MONGO_VERSION=r2.6.1
+#export MONGO_VERSION=r2.6.1
+export MONGO_VERSION=26compat
 
 if [ "$CHAOS_TARGET" == "BBB" ]; then
     export LMEM_VERSION=1.0.18
