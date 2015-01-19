cea7116602f5e90e371252d65167653c396ff5a3
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-01-20 00:12:09 +0100

    build cu controlled random conf

--- chaos_build_conf.sh
+++ chaos_build_conf.sh
@@ -8,17 +8,35 @@ fi
 
 randoms=()
 variables=`cat $confile | grep -o '\$r[0-9]*'|sort -n |uniq`
-expressions=`cat $confile | grep -o '\$r([0-9\.,]*)'`
+expressions=`cat $confile | grep -o '\$r[0-9]*([0-9\.,]*)'`
+
 
 for r in $variables;do
 rand=$RANDOM
-randoms+=($rand)
-done
-arr=()
-for r in $expressions;do
-    arr=`echo $r|tr ',' ' '` 
-    echo "$r => ${arr[0]} ${arr[1]} size: ${#arr[@]}"
-done
-if [[ "$var" =~ r(\d+)\((.+)\) ]]; then
-    echo "found r${BASH_REMATCH[1]} = ${BASH_REMATCH2}"
+if [[ "$r" =~ r([0-9]+) ]]; then
+    randoms[${BASH_REMATCH[1]}]=$rand;
+#    echo "r${BASH_REMATCH[1]}=$rand"
 fi
+#randoms+=($rand)
+done
+expression_value=()
+replace=""
+for r in $expressions;do
+    val=`echo $r|tr ',' ' '`
+    if [[ "$val" =~ r([0-9]+)\((.+)\) ]]; then
+	curr_arr=(${BASH_REMATCH[2]})
+	sel=$((${randoms[${BASH_REMATCH[1]}]}%${#curr_arr[@]}));
+	expression_value+=(${curr_arr[$sel]})
+	r_escape=`echo $r | sed 's/(/\\(/g' |sed 's/)/\\)/g'`
+#	echo "-$sel $r =${curr_arr[$sel]}"
+	if [ -z "$replace" ];then
+	    replace="cat $confile| sed 's/$r_escape/${curr_arr[$sel]}/g'";
+	else
+	    replace="$replace | sed 's/$r_escape/${curr_arr[$sel]}/g'";
+	fi
+    fi
+done
+
+eval $replace
+
+
