2ebbf10d1a9dae824d451b0ac01a9af847131710
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-07-22 10:03:48 +0200

    added directory selection

--- chaos_git.sh
+++ chaos_git.sh
@@ -13,9 +13,9 @@ outbase=$dir/../
 create_deb_ver=""
 remove_working="false"
 log="$0.log"
-exclude_dir=()
+on_dir=()
 
-git_dirs=$(find . -name ".git" -exec grep -sl opensource \{\}/config \;)
+
 
 mesg=""
 die(){
@@ -41,49 +41,86 @@ git_checkout(){
     return 0
    
 }
+git_arg=()
+git_cmd=""
 
-while getopts t:c:p:hs opt; do
+while getopts t:c:p:hsd: opt; do
     case $opt in
 	t) 
 	    echo -n "provide a tag message:"
 	    read mesg
+	    git_cmd=t
+	    ;;
+	d) 
+	    on_dir+=($OPTARG)
+	    ;;
+	s) 
+	    git_cmd=s
+	    ;;
+	c) 
+	    git_cmd=c
+	    git_arg=$OPTARG
+	    ;;
+	p) 
+	    git_cmd=p
+	    git_arg=$OPTARG
+	    ;;
+	h)
+	    echo -e "Usage is $0 [-s] [-t <tag name>][ -c <checkout branch> ] [ -p <branch name> ] [-d <directory0>] [-d <directory1>] \n-c <branch name>: check out a given branch name in all subdirs\n-p <branch>:commit and push modifications of a given branch\n-s:retrive the branch status\n-t <tag name>:make an annotated tag to all\n-d <directory>: apply just to the specified directory"
+	    exit 0;
 	    ;;
     esac
 
-    for d in $git_dirs; do
+done
+
+if [ ${#on_dir[@]} -eq 0 ]; then
+    dirs=$(find . -name ".git" -exec grep -sl opensource \{\}/config \;)
+    for d in $dirs;do
 	dir=$(dirname $d)
 	dir=$(dirname $dir)
-	pushd $dir > /dev/null
-	case $opt in
-	    s)
-		info_mesg "directory " "$dir"
-		git status 
-		;;
+	on_dir+=($dir)
 
-	    c)
-		git_checkout $dir $OPTARG 
-		;;
-	    t)
-		git tag -f -m "$mesg" $OPTARG
-		git push
-		;;
-	    p)
-		if git status | grep modified > /dev/null; then
-		    echo -n "modification found on \"$dir\" [branch:$OPTARG], empty comment to skip:"
-		    read mesg
-		    if [ -n "$mesg" ]; then
+    done
+fi
+
+if [ ${#on_dir[@]} -eq 0 ]; then
+    nok_mesg "no git dirs specified/found"
+    exit 1
+fi
+
+for dir in ${on_dir[@]}; do
+    pushd $dir > /dev/null
+    case $git_cmd in
+	s)
+	    git fetch
+	    info_mesg "directory " "$dir"
+	    git status 
+	    ;;
+	
+	c)
+	    git_checkout $dir $git_arg
+	    ;;
+	t)
+	    git fetch
+	    git pull
+	    tagname=$(echo "$git_arg" | tr ' ' '-')
+	    git tag -f -m "$mesg" $tagname
+	    git push
+	    ;;
+	p)
+	    if git status | grep modified > /dev/null; then
+		echo -n "modification found on \"$dir\" [branch:$git_arg], empty comment to skip:"
+		read mesg
+		if [ -n "$mesg" ]; then
 			info_mesg "committing changes in " "$dir"
 			git commit -m "$mesg" .
 			git push > /dev/null
-		    fi
 		fi
-		
-		;;
-	    h)
-		echo -e "Usage is $0 [-s] [-t <tag name>][ -c <checkout branch> ] [ -p <branch name> ]\n-c <branch name>: check out a given branch name in all subdirs\n-p <branch>:commit and push modifications of a given branch\n-s:retrive the branch status\n-t <tag name>:make an annotated tag to all"
-		exit 0;
-		;;
-	esac
-	popd > /dev/null
-    done
+	    fi
+	    
+	    ;;
+    esac
+
+    popd > /dev/null
 done
+
