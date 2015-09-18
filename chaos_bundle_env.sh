d4eff3256d0b951b91a59482fa3fbb5ed1fb651c
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-09-18 11:47:34 +0200

    use DISABLE_AND_SWAP for kernel < 2.6.35

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -84,8 +84,8 @@ else
 	    export CHAOS_EXCLUDE_DIR="oscilloscopes mongo chaos_services"
 	fi
 	export CHAOS_DISABLE_EVENTFD=true
-	export CFLAGS="$CFLAGS -march=armv5te -msoft-float -D__BSON_USEMEMCPY__ -DBOOST_ASIO_DISABLE_EVENTFD"
-	export CXXFLAGS="$CXXFLAGS -march=armv5te -msoft-float -D__BSON_USEMEMCPY__ -DBOOST_ASIO_DISABLE_EVENTFD"
+	export CFLAGS="$CFLAGS -march=armv5 -msoft-float -D__BSON_USEMEMCPY__ -DBOOST_ASIO_DISABLE_EVENTFD -mno-unaligned-access -DDISABLE_COMPARE_AND_SWAP -mfloat-abi=softfp -mtune=arm9"
+	export CXXFLAGS="$CXXFLAGS -march=armv5 -msoft-float -D__BSON_USEMEMCPY__ -DBOOST_ASIO_DISABLE_EVENTFD -mno-unaligned-access -DDISABLE_COMPARE_AND_SWAP -mfloat-abi=softfp -mtune=arm9"
 	export CHAOS_BOOST_VERSION=55
 	#
 	export CHAOS_BOOST_FLAGS="toolset=gcc-arm target-os=linux cxxflags=-DBOOST_ASIO_DISABLE_EVENTFD"
