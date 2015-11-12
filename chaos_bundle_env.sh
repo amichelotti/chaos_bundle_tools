12c1e3f2829d846908baa7d28c4b8ebde0103d42
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-11-12 12:05:31 +0100

    REMOVED .a from libzmq GENERATE A INCREDIBLE BUG INTO CMAKE

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -18,9 +18,9 @@ export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 
 #set default compile lib
 if [ $(uname -s) == "Darwin" ]; then
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq.a mongoose jsoncpp dl pthread"
+    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq mongoose jsoncpp dl pthread"
 else
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq.a mongoose jsoncpp dl pthread rt"
+    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq mongoose jsoncpp dl pthread rt"
 fi;
 
 export WEB_UI_SERVICE=$CHAOS_BUNDLE/service/webgui/CUiserver
