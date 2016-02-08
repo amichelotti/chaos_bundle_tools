381bb9dca6633881ee24fbd0b37ba90da6303ebb
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-02-08 14:53:18 +0100

    lib dl must be included

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -17,9 +17,9 @@ export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 
 #set default compile lib
 if [ $(uname -s) == "Darwin" ]; then
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq mongoose jsoncpp pthread"
+    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq mongoose jsoncpp pthread dl"
 else
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq mongoose jsoncpp pthread rt"
+    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq mongoose jsoncpp pthread rt dl"
 fi;
 
 export WEB_UI_SERVICE=$CHAOS_BUNDLE/service/webgui/CUiserver
@@ -154,7 +154,7 @@ if [ -z "$CHAOS_STATIC" ]; then
 export CHAOS_BOOST_FLAGS="$CHAOS_BOOST_FLAGS cxxflags=-fPIC"
 export CXXFLAGS="$CXXFLAGS -fPIC"
 export CFLAGS="$CFLAGS -fPIC"
-CHAOS_LINK_LIBRARY="$CHAOS_LINK_LIBRARY dl"
+
 fi
 
 
