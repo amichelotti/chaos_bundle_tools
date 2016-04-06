271e3d00cf2ff14bde2c5b4c64988ca68020afb7
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-04-06 18:13:51 +0200

    fix

--- config/lnf/production/deploy_beagle.sh
+++ config/lnf/production/deploy_beagle.sh
@@ -8,9 +8,9 @@ echo "* using $CHAOS_BUNDLE"
 cd $CHAOS_BUNDLE
 BASE=chaos-armhf-static-release
 ./tools/chaos_build.sh -t armhf -o static -b release
-./tools/chaos_remote_command.sh -u root -c "/etc/init.d/chaos-cu stop" tools/config/lnf/production/beagle_list.txt 
+./tools/chaos_remote_command.sh -u root -c "/etc/init.d/chaos-cu.sh stop" tools/config/lnf/production/beagle_list.txt 
 chaos_remote_copy.sh -u root -s $BASE/bin/UnitServer ./tools/config/lnf/production/beagle_list.txt
-chaos_remote_command.sh -u root -c "/etc/init.d/libera-cu.sh start" ./tools/config/lnf/production/beagle_list.txt 
+chaos_remote_command.sh -u root -c "/etc/init.d/chaos-cu.sh start" ./tools/config/lnf/production/beagle_list.txt 
 
 echo "Beagle successfully installed"
 
