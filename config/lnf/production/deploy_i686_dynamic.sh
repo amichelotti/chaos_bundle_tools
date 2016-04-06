3dbdbb987cdf1a08b6ab089b5c9f18aa73766a21
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-04-06 17:55:50 +0200

    fixed script

--- config/lnf/production/deploy_i686_dynamic.sh
+++ config/lnf/production/deploy_i686_dynamic.sh
@@ -8,11 +8,11 @@ echo "* using $CHAOS_BUNDLE"
 cd $CHAOS_BUNDLE
 BASE=chaos-i686-linux26-dynamic-release
 ./tools/chaos_build.sh -t i686-linux26 -o dynamic -b release 
-tar cvfz $BASE $BASE.tar.gz
-./tools/chaos_remote_command.sh -u root -c "service chaos-us stop" tools/config/lnf/production/i686_dynamic_list.txt 
-chaos_remote_copy.sh -u root -s $BASE.tar.gz ./tools/config/lnf/production/i686_tynamic_list.txt
+tar cvfz $BASE.tar.gz $BASE 
+./tools/chaos_remote_command.sh -u root -c "initctl stop chaos-us" tools/config/lnf/production/i686_dynamic_list.txt 
+./tools/chaos_remote_copy.sh -u root -s $BASE.tar.gz ./tools/config/lnf/production/i686_dynamic_list.txt
 ./tools/chaos_remote_command.sh -u root -c "tar xvfz $BASE.tar.gz" tools/config/lnf/production/i686_dynamic_list.txt 
-chaos_remote_command.sh -u root -c "service chaos-us start" ./tools/config/lnf/production/i686_dynamic_list.txt 
+./tools/chaos_remote_command.sh -u root -c "initctl start chaos-us" ./tools/config/lnf/production/i686_dynamic_list.txt 
 
 echo "i686 dynamic successfully installed"
 
