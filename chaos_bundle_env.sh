6c14a353376cfff49743de3f40a17c6cffbb81e8
amichelo <andrea.michelotti@lnf.infn.it>
2016-05-25 18:06:39 +0200

    fix to compile lua

--- chaos_bundle_env.sh
+++ chaos_bundle_env.sh
@@ -19,9 +19,9 @@ export PATH=$CHAOS_BUNDLE/tools:$CHAOS_BUNDLE/usr/local/bin:$PATH
 
 #set default compile lib
 if [ $(uname -s) == "Darwin" ]; then
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq mongoose jsoncpp pthread dl"
+    export CHAOS_LINK_LIBRARY="lua boost_program_options boost_date_time boost_system boost_thread boost_chrono boost_regex boost_log_setup boost_log boost_filesystem memcached zmq mongoose jsoncpp pthread dl"
 else
-    export CHAOS_LINK_LIBRARY="boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq mongoose jsoncpp pthread rt dl"
+    export CHAOS_LINK_LIBRARY="lua boost_program_options boost_date_time boost_system  boost_chrono boost_regex boost_log_setup boost_log boost_filesystem boost_thread boost_atomic memcached zmq mongoose jsoncpp pthread rt dl"
 fi;
 
 export WEB_UI_SERVICE=$CHAOS_BUNDLE/service/webgui/CUiserver
@@ -154,12 +154,13 @@ else
 fi
 
 export CHAOS_BOOST_FLAGS="$CHAOS_BOOST_FLAGS link=static"
+export CXXFLAGS="$CXXFLAGS -fPIC"
+export CFLAGS="$CFLAGS -fPIC"
 
 if [ -z "$CHAOS_STATIC" ]; then
 
 export CHAOS_BOOST_FLAGS="$CHAOS_BOOST_FLAGS cxxflags=-fPIC"
-export CXXFLAGS="$CXXFLAGS -fPIC"
-export CFLAGS="$CFLAGS -fPIC"
+
 
 fi
 
