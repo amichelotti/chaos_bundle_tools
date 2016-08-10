31c32175ac2d89c7eb0ad731c9a23ba3e30b0580
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-08-10 10:28:16 +0200

    removing or moving scripts

--- chaos_bundle_env_bbb.sh
+++
@@ -1,8 +0,0 @@
-export CHAOS_TARGET=armhf
-SOURCE="${BASH_SOURCE[0]}"
-
-pushd `dirname $SOURCE` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
-echo "CHAOS_TARGET: $CHAOS_TARGET"
-source $SCRIPTPATH/chaos_bundle_env.sh
