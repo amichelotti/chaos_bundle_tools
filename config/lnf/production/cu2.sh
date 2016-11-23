121140a20581fb963ec0eb3a99698cded90455ca
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-11-23 17:03:54 +0100

    production alignement

--- config/lnf/production/cu2.sh
+++ config/lnf/production/cu2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ulimit -u unlimited
 separator='-'
 pushd `dirname $0` > /dev/null
 dir=`pwd -P`
 popd > /dev/null
+
 
 if [ -z "$CHAOS_PREFIX" ];then
     echo "%% NO environment CHAOS_PREFIX defined, setting $prefix_tmp"
@@ -16,41 +16,42 @@ fi
 
 export LD_LIBRARY_PATH=$CHAOS_PREFIX/lib
 info_mesg "using prefix " "$CHAOS_PREFIX"
-check_proc_then_kill BPMSync
-check_proc_then_kill daqLiberaServer
-check_proc_then_kill UnitServer    
+kill_monitor_process
+
+
 procid=()
 cuid=()
 
-if launch_us_cu 1 1 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" UnitServer DAFNE/IMPORT;then
-    ok_mesg "US DAFNE_IMPORT $!"
-    procid+=($!)
-    cuid+=("DAFNE_IMPORT")
-else
-    nok_mesg "US DAFNE_IMPORT"
-    exit 1
-fi
 
-## runs on the VMIC
- # if launch_us_cu 1 1 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" UnitServer BTF/DAQ;then
- #     ok_mesg "US BTF/DAQ $!"
- #     procid+=($!)
- #     cuid+=("BTF/DAQ")
- # else
- #     nok_mesg "US BTF/DAQ"
- #     exit 1
- # fi
+ if launch_us_cu 1 1 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" UnitServer DAFNE/IMPORT;then
+     ok_mesg "US DAFNE_IMPORT $!"
+     procid+=($!)
+     cuid+=("DAFNE_IMPORT")
+ else
+     nok_mesg "US DAFNE_IMPORT"
+     exit 1
+ fi
 
-# if launch_us_cu 1 1 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" BPMSync ACCUMULATOR/BPM;then
-#     ok_mesg "US BPMsync $!"
+ if launch_us_cu 1 1 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" BPMSync ACCUMULATOR/BPM;then
+     ok_mesg "US BPMsync $!"
+     procid+=($!)
+     cuid+=("ACCUMULATOR/BPM")
+ else
+     nok_mesg "US BPMSync"
+     exit 1
+ fi
+
+
+
+# ## BENCHMARK
+# if launch_us_cu 1 10 "--conf-file $CHAOS_PREFIX/etc/cu.cfg" UnitServer BENCHMARK_UNIT_1;then
+#     ok_mesg "US Benchmark Unit $!"
 #     procid+=($!)
-#     cuid+=("ACCUMULATOR/BPM")
+#     cuid+=("BENCHMARK_UNIT_1")
 # else
-#     nok_mesg "US BPMSync"
+#     nok_mesg "BENCHMARK_UNIT_1"
 #     exit 1
 # fi
-
-
 
 
 info_mesg "monitoring cus"
