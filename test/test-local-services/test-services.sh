f24c20c72103dacfe93b271b132d1a2a10a85ce3
amichelo <andrea.michelotti@lnf.infn.it>
2017-09-02 20:50:14 +0200

    wget

--- test/test-local-services/test-services.sh
+++ test/test-local-services/test-services.sh
@@ -68,18 +68,18 @@ else
 fi
 status=0
 
-if which wget >& /dev/null ;then 
-    info_mesg "Testing UI Server"
-    unset http_proxy
-    if execute_command_until_ok "wget localhost:8081/CU?dev=BENCHMARK_UNIT_0/TEST_CU_0 -P wget_test1 " 10 ;then
-	ok_mesg "CUI answer"
-    else
-	nok_mesg "CUI answer"
-	end_test 1 "CUI answer"
-    fi
-else
-    info_mesg "skipping CUI test because wget is " "missing"
-fi
+# if which wget >& /dev/null ;then 
+#     info_mesg "Testing UI Server"
+#     unset http_proxy
+#     if execute_command_until_ok "wget localhost:8081/CU?dev=BENCHMARK_UNIT_0/TEST_CU_0 -P wget_test1 " 10 ;then
+# 	ok_mesg "CUI answer"
+#     else
+# 	nok_mesg "CUI answer"
+# 	end_test 1 "CUI answer"
+#     fi
+# else
+#     info_mesg "skipping CUI test because wget is " "missing"
+# fi
 sleep 1
 
 
