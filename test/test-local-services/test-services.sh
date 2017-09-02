432739861bb23412f70dc99304261b2bc7e96603
amichelo <andrea.michelotti@lnf.infn.it>
2017-09-02 17:16:17 +0200

    show what happen

--- test/test-local-services/test-services.sh
+++ test/test-local-services/test-services.sh
@@ -71,7 +71,7 @@ status=0
 if which wget >& /dev/null ;then 
     info_mesg "Testing UI Server"
     unset http_proxy
-    if execute_command_until_ok "wget localhost:8081/CU?dev=TEST_UNIT_0/TEST_CU_0 -P wget_test1 >& /dev/null" 10 ;then
+    if execute_command_until_ok "wget localhost:8081/CU?dev=BENCHMARK_UNIT_0/TEST_CU_0 -P wget_test1 " 10 ;then
 	ok_mesg "CUI answer"
     else
 	nok_mesg "CUI answer"
