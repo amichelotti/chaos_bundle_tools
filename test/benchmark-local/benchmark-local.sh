4968164b7a7c6fc9bd551c7a3fdd1ddbad0bc113
amichelo <andrea.michelotti@lnf.infn.it>
2017-09-05 11:23:24 +0200

    wait for all cus

--- test/benchmark-local/benchmark-local.sh
+++ test/benchmark-local/benchmark-local.sh
@@ -6,7 +6,7 @@ mkdir $CHAOS_PREFIX/vfs
 ## prepare a local random configuration for
 ## 2 US and 10 CU and CDS MODE 1 
 ../test-local-services/test-services.sh 2 10 "benchmark" 1 || exit 1
-./test-ping-bandwidth.sh 1 1 $CHAOS_MDS 1048576 UnitServer || exit 
+./test-ping-bandwidth.sh 1 52 $CHAOS_MDS 1048576 UnitServer || exit 
 
 ##    ./test-ping-bandwidth.sh 1 1 $CHAOS_MDS 1048576 MessMonitor || exit 
 
