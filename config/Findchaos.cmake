e49fba49cd5e52537c184889e2546ca2d3520e9e
amichelo <andrea.michelotti@lnf.infn.it>
2017-10-24 21:40:37 +0200

    chaos cmake package

---
+++ config/Findchaos.cmake
@@ -0,0 +1,5 @@
+set(PREFIX ${CMAKE_CURRENT_LIST_DIR})
+set(chaos_INCLUDE_DIRS ${PREFIX}/include)
+FILE(GLOB boost_libs ${PREFIX}/lib/libboost*.a)
+set(chaos_LIBRARIES ${PREFIX}/lib/libchaos_metadata_service_client.so ${PREFIX}/lib/libchaos_common.so ${PREFIX}/lib/libchaos_cutoolkit.so pthread dl)
+link_directories(${PREFIX}/lib)
