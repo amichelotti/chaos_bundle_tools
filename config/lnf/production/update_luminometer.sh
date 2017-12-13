9ff84da8b5bd62f9a906a2315c51e667fb0d7ff4
amichelo <andrea.michelotti@lnf.infn.it>
2017-12-13 09:52:08 +0100

    symbolic link not ok

--- config/lnf/production/update_luminometer.sh
+++ config/lnf/production/update_luminometer.sh
@@ -6,9 +6,9 @@ fi
 echo "* retriving static i686 binaries"
 wget -q http://opensource.lnf.infn.it/binary/chaos/$1/i686/chaos-distrib-$1-build_i686_static_linux26.tar.gz
 scp chaos-distrib-$1-build_i686_static_linux26.tar.gz root@dante073:
-ssh root@dante073 'initctl stop chaos-us'
+ssh root@dante073 'initctl stop chaos-us;rm -f /root/chaos-distrib-*.gz'
 ssh root@dante073 "tar xfz chaos-distrib-$1-build_i686_static_linux26.tar.gz chaos-distrib-$1-build_i686_static_linux26/bin/UnitServer"
-ssh root@dante073 "ln -sf /root/chaos-distrib-$1-build_i686_static_linux26 /usr/local/chaos-distrib"
+ssh root@dante073 "rm -f /usr/local/chaos-distrib;ln -s /root/chaos-distrib-$1-build_i686_static_linux26 /usr/local/chaos-distrib"
 sleep 5
 ssh root@dante073 'initctl start chaos-us'
 
