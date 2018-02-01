845b2df5629237603580c054fa2dda936b6d9461
amichelo <andrea.michelotti@lnf.infn.it>
2018-02-01 13:56:22 +0100

    optimized luminometer deploy

--- config/lnf/production/update_luminometer.sh
+++ config/lnf/production/update_luminometer.sh
@@ -4,12 +4,17 @@ if [ -z "$1" ];then
     exit 1
 fi
 echo "* retriving static i686 binaries"
-wget -q http://opensource.lnf.infn.it/binary/chaos/$1/i686/chaos-distrib-$1-build_i686_static_linux26.tar.gz
-scp chaos-distrib-$1-build_i686_static_linux26.tar.gz root@dante073:
+# wget  http://opensource.lnf.infn.it/binary/chaos/$1/i686/chaos-distrib-$1-build_i686_static_linux26.tar.gz
+echo "* open package"
+tar xvfz chaos-distrib-$1-build_i686_static_linux26.tar.gz chaos-distrib-$1-build_i686_static_linux26/bin/UnitServer
+echo "* stopping services"
 ssh root@dante073 'initctl stop chaos-us;rm -f /root/chaos-distrib-*.gz'
-ssh root@dante073 "tar xfz chaos-distrib-$1-build_i686_static_linux26.tar.gz chaos-distrib-$1-build_i686_static_linux26/bin/UnitServer"
+echo "* updating.."
+scp -r chaos-distrib-$1-build_i686_static_linux26/bin/UnitServer root@dante073:
+echo "* relink.."
 ssh root@dante073 "rm -f /usr/local/chaos-distrib;ln -s /root/chaos-distrib-$1-build_i686_static_linux26 /usr/local/chaos-distrib"
 sleep 5
+echo "* restart.."
 ssh root@dante073 'initctl start chaos-us'
 
 
