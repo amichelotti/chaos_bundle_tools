c57eadae9a2cb441a6dfabc20259872d79f64009
amichelo <andrea.michelotti@lnf.infn.it>
2018-07-19 14:46:27 +0200

    mess client

--- dbinit.sh
+++ dbinit.sh
@@ -4,7 +4,7 @@ curl -u Administrator:chaos2015 -v -X POST http://$1:8091/node/controller/setupS
 curl -v -X POST http://$1:8091/nodes/self/controller/settings -d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&index_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata'
 
 curl -v -X POST http://$1:8091/settings/web -d 'password=chaos2015&username=Administrator&port=SAME'
-curl -X POST -u Administrator:chaos2015 -d 'name=chaos' -d 'bucketType=memcached' -d 'ramQuotaMB=512' -d 'authType=sasl'  -d 'replicaNumber=0' -d 'saslPassword=chaos' http://$1:8091/pools/default/buckets
+curl -X POST -u Administrator:chaos2015 -d 'name=chaos' -d 'ramQuotaMB=512' -d 'authType=sasl'  -d 'replicaNumber=0' -d 'saslPassword=chaos' http://$1:8091/pools/default/buckets
 curl -u Administrator:chaos2015 http://$1:8091/pools/default/buckets
 
 echo "* init DB server $2"
