43c9bff14073ab4cf79018bfebb4dc748cfdb218
amichelo <andrea.michelotti@lnf.infn.it>
2018-09-19 16:43:15 +0200

    updated test with performance

--- test/benchmark-local/benchmark-local.sh
+++ test/benchmark-local/benchmark-local.sh
@@ -9,11 +9,16 @@ mkdir $CHAOS_PREFIX/vfs
 ./test-ping-bandwidth.sh 1 52 $CHAOS_MDS 524288 UnitServer || exit 
 
 ##    ./test-ping-bandwidth.sh 1 1 $CHAOS_MDS 1048576 MessMonitor || exit 
+$CHAOS_PREFIX/tools/chaos_services.sh stop webui
 
-
-if run_proc "$CHAOS_PREFIX/bin/misc/testDataSetIO --metadata-server $CHAOS_MDS --loop 1000000 --points 100 --log-on-file 1 --log-level debug --log-file $CHAOS_PREFIX/log/testDataSetIO.log --direct-io-client-kv-param=ZMQ_RCVTIMEO:600000
+if run_proc "$CHAOS_PREFIX/bin/misc/testDataSetIO --metadata-server $CHAOS_MDS --points 0 --pointmax 20000 --pointincr 2 --report $CHAOS_PREFIX/log/testDataSetIO.csv --loop 1000 --log-on-file 1 --log-file $CHAOS_PREFIX/log/testDataSetIO.log --direct-io-client-kv-param=ZMQ_RCVTIMEO:600000
 " "testDataSetIO";then
     ok_mesg "testDataSetIO"
+    pushd $CHAOS_PREFIX/log
+    if gnuplot < $CHAOS_PREFIX/etc/testio.gnuplot ;then
+		info_mesg "generated " " $CHAOS_PREFIX/log/testDataSetIO.png"
+	fi
+    popd
 else
     nok_mesg "testDataSetIO"
 fi
