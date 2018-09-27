cca8b5f5fae38c4ffd6c6eef98adcdb77181edb4
amichelo <andrea.michelotti@lnf.infn.it>
2018-09-27 16:31:06 +0200

    improved driver testio

--- testio.sh
+++ testio.sh
@@ -14,28 +14,29 @@ loop=10000
 if [ -n "$2" ];then
     loop=$2
 fi
+host=`hostname`
 echo "set datafile separator \",\"" > testioMulti.gnuplot  
 #set yrange [1:150000]
 echo "#set logscale y 2" >> testioMulti.gnuplot  
 echo "set logscale x 2" >> testioMulti.gnuplot  
 echo "set xlabel 'Bytes'" >> testioMulti.gnuplot  
 echo "set terminal png size 2048,4096 enhanced font 'Verdana,10'" >> testioMulti.gnuplot  
-echo "set output 'testDataSetIOMulti.png'">> testioMulti.gnuplot  
+echo "set output 'testDataSetIOMulti-$host.png'">> testioMulti.gnuplot  
+mach=`uname -a`
 
-echo "set multiplot layout 8,1 title 'Infrastructure Client Test' font ',14'" >> testioMulti.gnuplot  
+echo "set multiplot layout 8,1 title '$mach on $1, loops $loop' font ',14'" >> testioMulti.gnuplot  
 
 echo "* starting performace test on $1"
 nthreads="1 2 3 4 5 6 7 8"
 for i in $nthreads;do
     echo "Starting test with $i threads"
     echo "set title '$i threads'">> testioMulti.gnuplot  
-    echo "plot 'testDataSetIO_$i.csv' using 2:3 lc rgb \"green\" with lines title 'push rate (cycle/s)','testDataSetIO_$i.csv' using 2:4 lc rgb \"cyan\" with lines title 'pull rate (cycle/s)', 'testDataSetIO_$i.csv' using 2:10 lc rgb \"red\" with lines title  'errors'" >> testioMulti.gnuplot  
-    $CHAOS_PREFIX/bin/testDataSetIO --points 0 --pointmax 10000 --metadata-server $1 --nthread $i --pointincr 2 --loop $loop --report testDataSetIO_$i.csv
+    echo "plot 'testDataSetIO-$host-$i.csv' using 2:3 lc rgb \"green\" with lines title 'push rate (cycle/s)','testDataSetIO-$host-$i.csv' using 2:4 lc rgb \"cyan\" with lines title 'pull rate (cycle/s)', 'testDataSetIO-$host-$i.csv' using 2:10 lc rgb \"red\" with lines title  'errors'" >> testioMulti.gnuplot  
+    $CHAOS_PREFIX/bin/testDataSetIO --points 0 --pointmax 10000 --metadata-server $1 --nthread $i --pointincr 2 --loop $loop --report testDataSetIO-$host-$i.csv --log-on-file --log-file testDataSetIO-$host-$i.log --log-level debug
     
-#    echo "plot 'testDataSetIO_$i.csv' using 2:3 lc rgb \"green\" with lines title 'push rate (cycle/s)','testDataSetIO_$i.csv' using 2:4 lc rgb \"cyan\" with lines title 'pull rate (cycle/s)','testDataSetIO_$i.csv' using 2:8 lc rgb \"pink\" with lines title 'bandwith (MB/s)','testDataSetIO_$i.csv' using 2:9 lc rgb \"magenta\" with lines title  'prep overhead(us)', 'testDataSetIO_$i.csv' using 2:10 lc rgb \"red\" with lines title  'errors'" >> testioMulti.gnuplot  
+#    echo "plot 'testDataSetIO-$host-$i.csv' using 2:3 lc rgb \"green\" with lines title 'push rate (cycle/s)','testDataSetIO-$host-$i.csv' using 2:4 lc rgb \"cyan\" with lines title 'pull rate (cycle/s)','testDataSetIO-$host-$i.csv' using 2:8 lc rgb \"pink\" with lines title 'bandwith (MB/s)','testDataSetIO-$host-$i.csv' using 2:9 lc rgb \"magenta\" with lines title  'prep overhead(us)', 'testDataSetIO-$host-$i.csv' using 2:10 lc rgb \"red\" with lines title  'errors'" >> testioMulti.gnuplot  
     
 
-    echo "Sleeping 60"
-    sleep 60
+    sleep 1
 done
 echo "unset multiplot">> testioMulti.gnuplot  
