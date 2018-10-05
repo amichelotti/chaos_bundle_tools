865fc17cd425cdb74d9ab28e804e5dc73da68ced
amichelo <andrea.michelotti@lnf.infn.it>
2018-10-05 16:39:26 +0200

    support for couchbase5

--- dbinit.sh
+++ dbinit.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 echo "* init couchbase server $1 db $2"
-curl -u Administrator:chaos2015 -v -X POST http://$1:8091/node/controller/setupServices -d 'services=data'
+curl -u chaos:chaos2015 -v -X POST http://$1:8091/node/controller/setupServices -d 'services=data'
 curl -v -X POST http://$1:8091/nodes/self/controller/settings -d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&index_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata'
 
-curl -v -X POST http://$1:8091/settings/web -d 'password=chaos2015&username=Administrator&port=SAME'
-curl -X POST -u Administrator:chaos2015 -d 'name=chaos' -d 'ramQuotaMB=512' -d 'authType=sasl'  -d 'replicaNumber=0' -d 'saslPassword=chaos' http://$1:8091/pools/default/buckets
-curl -u Administrator:chaos2015 http://$1:8091/pools/default/buckets
+curl -v -X POST http://$1:8091/settings/web -d 'password=chaos2015&username=chaos&port=SAME'
+
+echo "Setting couchbase chaos bucket"
+curl -f --silent --output /dev/null --show-error -X POST -u chaos:chaos2015 -d 'name=chaos' -d 'ramQuotaMB=512' -d "bucketType=ephemeral" -d 'authType=sasl' -d 'replicaNumber=0' -d 'saslPassword=chaoschaos' http://$1:8091/pools/default/buckets
+
+#curl -X POST -u Administrator:chaos2015 -d 'name=chaos' -d "bucketType=ephemeral" -d 'ramQuotaMB=512' -d 'authType=sasl'  -d 'replicaNumber=0' -d 'saslPassword=chaos' http://$1:8091/pools/default/buckets
+#curl -u Administrator:chaos2015 http://$1:8091/pools/default/buckets
 
 echo "* init DB server $2"
 rm -rf /tmp/dbinit
--- start_test_with_asan.sh
+++ start_test_with_asan.sh
@@ -1,1 +1,1 @@
-CHAOS_SERVICE_ENV="LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5" ./tools/chaos_test.sh . 
+CHAOS_SERVICE_ENV="LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5" ./tools/chaos_test.sh -k . 
