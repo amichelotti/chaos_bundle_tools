5687b9e24fcfc0deab9dd23f8acc2ea194a0404a
amichelo <andrea.michelotti@lnf.infn.it>
2019-05-23 10:54:02 +0200

    removed chaos_dashboard from webui

--- deploy_libera.sh
+++ deploy_libera.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-wget https://opensource.lnf.infn.it/binary/chaos/experimental/arm/chaos-distrib-experimental-build_arm_linux26.tar.gz
+RELEASE=$1
+if [ -z "$RELEASE" ];then
+    RELEASE="experimental"
+fi
+echo " Release $RELEASE"
+wget https://opensource.lnf.infn.it/binary/chaos/$EXPERIMENTAL/arm/chaos-distrib-$EXPERIMENTAL-build_arm_linux26.tar.gz
 echo "* extracting"
-tar xvfz chaos-distrib-experimental-build_arm_linux26.tar.gz chaos-distrib-experimental-build_arm_linux26/bin/daqLiberaServer
+tar xvfz chaos-distrib-$EXPERIMENTAL-build_arm_linux26.tar.gz chaos-distrib-$EXPERIMENTAL-build_arm_linux26/bin/daqLiberaServer
 echo "* copying"
-scp -v chaos-distrib-experimental-build_arm_linux26/bin/daqLiberaServer michelo@192.168.143.252:/export/chaos-libera/old
+scp -v chaos-distrib-$EXPERIMENTAL-build_arm_linux26/bin/daqLiberaServer michelo@192.168.143.252:/export/chaos-libera/old
 echo "* removing tar"
-rm -rf chaos-distrib-experimental-build_arm_linux26*
+rm -rf chaos-distrib-$EXPERIMENTAL-build_arm_linux26*
