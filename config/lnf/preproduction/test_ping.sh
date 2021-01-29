cd5618afcd1126e97781d90e1a7649a4c7b39a37
amichelo <andrea.michelotti@lnf.infn.it>
2021-01-29 10:23:59 +0100

    test ping

---
+++ config/lnf/preproduction/test_ping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ping chaost-cds01.chaos.lnf.infn.it
+ping chaost-cds02.chaos.lnf.infn.it
+ping chaost-cds03.chaos.lnf.infn.it
+ping chaost-webui01.chaos.lnf.infn.it
+ping chaost-webui02.chaos.lnf.infn.it
+ping chaost-webui03.chaos.lnf.infn.it
+ping chaost-cu01.chaos.lnf.infn.it
+ping chaost-cu02.chaos.lnf.infn.it
+ping chaost-cu03.chaos.lnf.infn.it
+
